268b9644d727666c3add4524fe4fc8897798efdb
OlgaTeg <tegza.o@yandex.ru>
2022-12-29 20:42:02 +0300

    Initial Commit

---
+++ RealmApp/ViewControllers/TaskListViewController.swift
@@ -0,0 +1,171 @@
+//
+//  TaskListsViewController.swift
+//  RealmApp
+//
+//  Created by Alexey Efimov on 02.07.2018.
+//  Copyright © 2018 Alexey Efimov. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class TaskListViewController: UITableViewController {
+
+    private var taskLists: Results<TaskList>!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        taskLists = StorageManager.shared.realm.objects(TaskList.self)
+        createTempData()
+        
+        let addButton = UIBarButtonItem(
+            barButtonSystemItem: .add,
+            target: self,
+            action: #selector(addButtonPressed)
+        )
+        navigationItem.rightBarButtonItem = addButton
+        navigationItem.leftBarButtonItem = editButtonItem
+    }
+    
+// MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let indexPath = tableView.indexPathForSelectedRow else { return }
+        guard let tasksVC = segue.destination as? TasksViewController else { return }
+        let taskList = taskLists[indexPath.row]
+        tasksVC.taskList = taskList
+    }
+
+// MARK: - Actions
+    @IBAction func sortingList(_ sender: UISegmentedControl) {
+        
+        if sender.selectedSegmentIndex == 0 {
+            taskLists = taskLists.sorted(byKeyPath: "name", ascending: true)
+        } else {
+            taskLists = taskLists.sorted(byKeyPath: "date", ascending: false)
+        }
+        tableView.reloadData()
+    }
+    
+    @objc private func  addButtonPressed() {
+        showAlert()
+    }
+}
+
+// MARK: - Table view Data Source
+extension TaskListViewController {
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        taskLists.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskListCell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        let taskList = taskLists[indexPath.row]
+        content.text = taskList.name
+        content.secondaryText = "\(taskList.tasks.count)"
+        cell.contentConfiguration = content
+        cell.configurateCell(with: taskList)
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let taskList = taskLists[indexPath.row]
+        
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { _, _, _ in
+            StorageManager.shared.delete(taskList)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+        
+        let editAction = UIContextualAction(style: .normal, title: "Edit") { [unowned self] _, _, isDone in
+            showAlert(with: taskList) {
+                tableView.reloadRows(at: [indexPath], with: .automatic)
+            }
+            isDone(true)
+        }
+        
+        let doneAction = UIContextualAction(style: .normal, title: "Done") { _, _, isDone in
+            StorageManager.shared.done(taskList)
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+            isDone(true)
+        }
+        
+        editAction.backgroundColor = .orange
+        doneAction.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+        
+        return UISwipeActionsConfiguration(actions: [doneAction, editAction, deleteAction])
+    }
+}
+
+//MARK: - Private methods
+extension TaskListViewController {
+    
+    private func createTempData() {
+        if !UserDefaults.standard.bool(forKey: "done") {
+            DataManager.shared.createTempData { [unowned self] in
+                UserDefaults.standard.set(true, forKey: "done")
+                tableView.reloadData()
+            }
+        }
+    }
+    
+    private func showAlert(with taskList: TaskList? = nil, completion: (() -> Void)? = nil) {
+        let title = taskList != nil ? "Edit List" : "New List"
+        let alert = UIAlertController.createAlert(withTitle: title, andMessage: "Please set title for new task list")
+        
+        alert.action(with: taskList) { [weak self] newValue in
+            if let taskList = taskList, let completion = completion {
+                StorageManager.shared.edit(taskList, newValue: newValue)
+                completion()
+            } else {
+                self?.save(taskList: newValue)
+            }
+        }
+        
+        present(alert, animated: true)
+    }
+    
+    private func save(taskList: String) {
+        StorageManager.shared.save(taskList) { taskList in
+            let rowIndex = IndexPath(row: taskLists.index(of: taskList) ?? 0, section: 0)
+            tableView.insertRows(at: [rowIndex], with: .automatic)
+        }
+    }
+}
+
+//MARK: - Configurate Cell
+
+extension UITableViewCell {
+    
+    func configurateCell(with taskLists: TaskList) {
+        
+        let currentTask = taskLists.tasks.filter("isComplete = false")
+        let comletedTask = taskLists.tasks.filter("isComplete = true")
+        let allTask = currentTask.count + comletedTask.count
+        
+        var content = defaultContentConfiguration()
+        
+        content.text = taskLists.name
+        
+        func getName(){
+            content.secondaryText = "\(currentTask.count)/\(allTask)"
+        }
+        
+        if !currentTask.isEmpty {
+            getName()
+            accessoryType = .none
+        } else if !comletedTask.isEmpty {
+            getName()
+            accessoryType = .checkmark
+            content.secondaryText = ""
+        } else {
+            accessoryType = .none
+            getName()
+        }
+        contentConfiguration = content
+    }
+}
